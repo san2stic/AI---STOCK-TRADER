2ce44ce82ea000a6b0323984e73f7dbab0509a7c
Bastien Javaux <0xmagicduck@proton.me>
2025-12-25 13:44:37 +0100

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
     reactStrictMode: true,
     output: 'standalone',
+    async rewrites() {
+        return [
+            {
+                source: '/api/models/:path*',
+                destination: process.env.API_URL ? `${process.env.API_URL}/api/models/:path*` : 'http://backend:8000/api/models/:path*',
+            },
+            {
+                source: '/api/learning/:path*',
+                destination: process.env.API_URL ? `${process.env.API_URL}/api/learning/:path*` : 'http://backend:8000/api/learning/:path*',
+            },
+            {
+                source: '/api/:path*',
+                destination: process.env.API_URL ? `${process.env.API_URL}/:path*` : 'http://backend:8000/:path*',
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
